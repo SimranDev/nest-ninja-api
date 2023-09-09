9379a58df4976d876252287657478ff29924281d
Simran Singh <simran.develop@gmail.com>
2023-09-09 22:14:26 +1200

    ninja controller routes added

--- src/ninjas/ninjas.controller.ts
+++ src/ninjas/ninjas.controller.ts
@@ -1,4 +1,41 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { CreateNinjaDto } from './dto/create-ninja.dto';
 
 @Controller('ninjas')
-export class NinjasController {}
+export class NinjasController {
+  //GET /ninjas --> []
+  @Get()
+  getNinjas() {
+    return [];
+  }
+  //GET /ninjas/:id --> { ... }
+  @Get(':id')
+  getOneNinja(@Param('id') id: string) {
+    return { id };
+  }
+  //POST /ninjas --> { ... }
+  @Post()
+  createNinja(@Body() body: CreateNinjaDto) {
+    return {
+      name: body.name,
+    };
+  }
+  //PUT /ninjas/:id --> { ... }
+  @Put(':id')
+  updateNinja() {
+    return {};
+  }
+  //DELETE /ninjas/:id --> { ... }
+  @Delete(':id')
+  deleteNinja() {
+    return {};
+  }
+}
